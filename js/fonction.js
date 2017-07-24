993df7b572695dd222f75989b54fa774a797490c
Vassily Dubois <vassily.dubois@gmail.com>
2017-07-25 00:25:23 +0200

    Ajout clic critique movible (manque commentaire

--- js/fonction.js
+++ js/fonction.js
@@ -14,17 +14,18 @@ function nbToStr(nb) {
 	return nb + ' ' + multiple[i] + tailStr;
 }
 
-function scoreInc() {
-	data.score += data.multiplier;
+function scoreInc(adder = 0, mult = 1) {
+	data.score += ((data.multiplier + adder) * mult) * data.bMult;
+	btDisplay.innerHTML = nbToStr(data.score);
 }
 
 function augmenterMultiplicateur() {
 	if(data.score >= data.mCost) {
 		data.score -= data.mCost;
-		data.multiplier += data.mLvl;
-		data.mLvl++;
+		btDisplay.innerHTML = nbToStr(data.score);
+		data.multiplier += 1;
 		data.mCost *= 2;
-			btMultiply.innerHTML = 'x' + nbToStr(data.multiplier) + ' multiplier<br>Next (+' + nbToStr(data.mLvl) + ') : ' + nbToStr(data.mCost);
+		btMultiply.innerHTML = 'x' + nbToStr(data.multiplier * data.bMult) + ' multiplier<br>Next : ' + nbToStr(data.mCost);
 	}
 }
 
@@ -33,6 +34,7 @@ function activateAutoClic() {
 		myAutoClic = setInterval(scoreInc, data.aTimer);
 		if(myAutoClic != 0) {
 			data.score -= data.aCost;
+			btDisplay.innerHTML = nbToStr(data.score);
 			data.aTimer = Math.round(data.aTimer * 0.95);
 			data.aCost *= 2;
 			data.aLvl++;
@@ -49,9 +51,7 @@ function timerWait() {
 	}
 	else {
 		bonusTimer = 0;
-		data.bonus += 6 * data.bLvl;
 		data.bTimer++;
-		data.bClic += data.bLvl;
 		data.bCost = Math.round(1.1 * data.bCost);
 		btBonus.innerHTML = '*!* BONUS *!*<br>' + nbToStr(data.bCost);
 		data.bLvl++;
@@ -62,13 +62,11 @@ function timerBonus() {
 	if(bonusTimer > 1) {
 		bonusTimer--;
 		btBonus.innerHTML = '*!* BONUS *!*<br>' + bonusTimer + 's';
-		data.score += (data.bonus/data.bTimer);
 		myBonus = setTimeout(timerBonus, 1000);
 	}
 	else {
-		data.score += (data.bonus/data.bTimer);
-		data.multiplier -= data.bClic;
-			btMultiply.innerHTML = 'x' + nbToStr(data.multiplier) + ' multiplier<br>Next (+' + nbToStr(data.mLvl) + ') : ' + nbToStr(data.mCost);
+		data.bMult = 1;
+		btMultiply.innerHTML = 'x' + nbToStr(data.multiplier * data.bMult) + ' multiplier<br>Next (+' + nbToStr(data.mLvl) + ') : ' + nbToStr(data.mCost);
 		bonusTimer = 30;
 		myBonus = setTimeout(timerWait, 1000);
 		btBonus.innerHTML = '*!* Wait *!*<br>' + bonusTimer + 's';
@@ -81,15 +79,16 @@ function activateBonus() {
 		if(myBonus != 0) {
 			bonusTimer = data.bTimer;
 			data.score -= data.bCost;
-			data.multiplier += data.bClic;
-			btMultiply.innerHTML = 'x' + nbToStr(data.multiplier) + ' multiplier<br>Next (+' + nbToStr(data.mLvl) + ') : ' + nbToStr(data.mCost);
+			btDisplay.innerHTML = nbToStr(data.score);
+			data.bMult = 2 * ((100 + data.bLvl)/100);
+			btMultiply.innerHTML = 'x' + nbToStr(data.multiplier * data.bMult) + ' multiplier<br>Next (+' + nbToStr(data.mLvl) + ') : ' + nbToStr(data.mCost);
 			btBonus.innerHTML = '*!* BONUS *!*<br>' + bonusTimer + 's';
 		}
 	}
 }
 
 function checkInterval() {
-	btDisplay.innerHTML = nbToStr(Math.floor(data.score));
+	btDisplay.innerHTML = nbToStr(data.score);
 
 	if(!btAutoClic.disabled && data.score < data.aCost) {
 		btAutoClic.disabled = true;
@@ -117,8 +116,66 @@ function checkInterval() {
 		btBonus.disabled = false;
 	}
 }
-/*
-/**
+
+function getPosition(element)
+{
+	var left = 0;
+	var top = 0;
+	//On récupère l'élément
+	var e = document.getElementById(element);
+	//Tant que l'on a un élément parent
+	while (e.offsetParent != undefined && e.offsetParent != null)
+	{
+		//On ajoute la position de l'élément parent
+		left += e.offsetLeft + (e.clientLeft != null ? e.clientLeft : 0);
+		top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
+		e = e.offsetParent;
+	}
+	return new Array(left,top);
+}
+
+function calcDist(item1, item2) {
+	var diffX = Math.abs(item1[0] - item2[0]);
+	var diffY = Math.abs(item1[1] - item2[1]);
+	return Math.sqrt(Math.pow(diffX, 2) + Math.pow(diffY, 2));
+}
+
+function myMove() {
+    var elem = document.getElementById("critical");
+    var posX = 0;
+    var posY = 0;
+    do {
+	    var pasX = (Math.random() * 2) - 1;
+	    var pasY = (Math.random() * 2) - 1;
+	}while(Math.sqrt(Math.pow(pasX, 2) + Math.pow(pasY, 2)) <= 0.75);
+    var id = setInterval(frame, 5);
+    function frame() {
+    	var distance = calcDist(myPos['clicker'], myPos['critical']);
+    	if(distance > btClic.offsetHeight/2 - btCritic.offsetHeight/2) {
+		    posX = (Math.random() * btClic.offsetHeight) - btClic.offsetHeight/2;
+		    posY = (Math.random() * btClic.offsetHeight) - btClic.offsetHeight/2;
+		    do {
+			    pasX = (Math.random() * 2) - 1;
+			    pasY = (Math.random() * 2) - 1;
+			}while(Math.sqrt(Math.pow(pasX, 2) + Math.pow(pasY, 2)) <= 0.75);
+    	}
+    	//btCritic.innerHTML = distance;
+    	//btClic.innerHTML = myPos['critical'] + '<br>' + myPos['clicker'];
+        posX += pasX;
+        posY += pasY;
+        elem.style.left = myPos['clicker'][0] + posX + 'px';
+        elem.style.top = myPos['clicker'][1] + posY + 'px';
+        myPos['clicker'] = [btClic.offsetLeft + btClic.offsetWidth/2, btClic.offsetTop + btClic.offsetHeight/2];
+		myPos['critical'] = [btCritic.offsetLeft + btCritic.offsetWidth/2, btCritic.offsetTop + btCritic.offsetHeight/2];
+    }
+}
+
+
+/***********************************************************************************
+/* Ce qui suit sert à mettre en place un autoclicker autonome pour cookie clicker **
+/***********************************************************************************
+*
+// Sert à récupérer la position d'un élément
 * @author Patrick Poulain
 * @see http://petitchevalroux.net
 * @licence GPL
@@ -141,19 +198,22 @@ function getPosition(element)
 }
 var myPos = getPosition("bigCookie");
 
+
+// Simule un évènement de type clic sur le cookie
 function simulateClick() {
 	var evt = new MouseEvent('click', {
 		view: window,
 		bubbles: true,
 		cancelable: true,
-		clientX: 250,
-		clientY: 250
 	});
 	var cb = document.getElementById("bigCookie"); 
 	var canceled = cb.dispatchEvent(evt);
 }
+
+// Permet de mettre fin à l'autoclic quand on évolue
 var testtt = document.getElementById("legacyButton");
 testtt.onmousedown = function(){clearInterval(myAutoClic)};
 
+// Lance l'autoclic
 var myAutoClic = setInterval(simulateClick, 10);
 */
--- js/main.js
+++ js/main.js
@@ -3,14 +3,26 @@ var btMultiply = document.getElementById("multiply");
 var btDisplay = document.getElementById("display");
 var btAutoClic = document.getElementById("autoclic");
 var btBonus = document.getElementById("bonus");
+var btCritic = document.getElementById("critical");
 var myAutoClic = 0;
 var myBonus = 0;
 var bonusTimer = 0;
-var data = {score:10000, multiplier:1, mCost:50, mLvl:1, aTimer:1000, aCost:500, aLvl:1, bonus:200, bTimer:30, bClic:3, bCost:5000, bLvl:1};
+var data = {score:0, multiplier:1, mCost:50, aTimer:1000, aCost:500, aLvl:1, bTimer:30, bMult:1, bCost:5000, bLvl:0, cHit:0};
 var autoCheck = setInterval(checkInterval, 100);
+var myPos = {};
 
 //myButton.onclick = scoreInc(score);
-btClic.addEventListener("click", scoreInc);
+btClic.addEventListener("click", function(){
+	scoreInc();
+	data.cHit = 0;
+});
 btMultiply.addEventListener("click", augmenterMultiplicateur);
 btAutoClic.addEventListener("click", activateAutoClic);
 btBonus.addEventListener("click", activateBonus);
+btCritic.addEventListener("click", function(){
+	scoreInc(data.cHit, 3);
+	data.cHit++;
+});
+myPos['clicker'] = [btClic.offsetLeft + btClic.offsetWidth/2, btClic.offsetTop + btClic.offsetHeight/2];
+myPos['critical'] = [btCritic.offsetLeft + btCritic.offsetWidth/2, btCritic.offsetTop + btCritic.offsetHeight/2];
+myMove();
